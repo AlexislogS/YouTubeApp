f0de7776961586699cf0f3b5af11a57cc81e8945
AlexislogS <55928329+AlexislogS@users.noreply.github.com>
2020-09-26 21:31:51 +0300

    Initial Commit

---
+++ YouTubeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTubeApp
+//
+//  Created by Alex Yatsenko on 26.09.2020.
+//  Copyright © 2020 AlexislogS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
